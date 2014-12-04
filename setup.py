6da81f21153eab1a02c0c575f3fd501b97228e3b
Florent Lebreton <florentlebreton@free.fr>
2014-12-04 13:58:40 +0100

    Release 0.8

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-jsignature',
-    version='0.7.6',
+    version='0.8',
     author='Florent Lebreton',
     author_email='florent.lebreton@makina-corpus.com',
     url='https://github.com/fle/django-jsignature',
-    download_url="https://github.com/fle/django-jsignature/tarball/0.7.6",
-    description="Use jSignature jQuery plugin in your django projects",
+    download_url='https://github.com/fle/django-jsignature/tarball/0.8',
+    description='Use jSignature jQuery plugin in your django projects',
     long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
-		     open(os.path.join(here, 'CHANGES')).read(),
+        open(os.path.join(here, 'CHANGES')).read(),
     license='LPGL, see LICENSE file.',
     install_requires=['Django'],
     packages=find_packages(),
@@ -26,5 +26,13 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 2.7'],
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.6',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.2',
+         'Programming Language :: Python :: 3.3',
+         'Programming Language :: Python :: 3.4'
+    ],
 )
