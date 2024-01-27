868fb53ade75e71c14bb3f6b963211930fcc9ecf
SebCorbin <sebastien@rgoods.com>
2024-01-27 23:27:40 +0100

    Draw single points

--- jsignature/utils.py
+++ jsignature/utils.py
@@ -6,19 +6,19 @@ import json
 from itertools import chain
 from PIL import Image, ImageDraw, ImageOps, __version__ as PIL_VERSION
 
-AA = 5  # super sampling gor antialiasing
+AA = 5  # super sampling for antialiasing
 
 
 def draw_signature(data, as_file=False):
-    """ Draw signature based on lines stored in json_string.
-        `data` can be a json object (list in fact) or a json string
-        if `as_file` is True, a temp file is returned instead of Image instance
+    """Draw signature based on lines stored in json_string.
+    `data` can be a json object (list in fact) or a json string
+    if `as_file` is True, a temp file is returned instead of Image instance
     """
 
     def _remove_empty_pts(pt):
         return {
-            'x': list(filter(lambda n: n is not None, pt['x'])),
-            'y': list(filter(lambda n: n is not None, pt['y']))
+            "x": list(filter(lambda n: n is not None, pt["x"])),
+            "y": list(filter(lambda n: n is not None, pt["y"])),
         }
 
     if isinstance(data, str):
@@ -30,26 +30,39 @@ def draw_signature(data, as_file=False):
 
     # Compute box
     padding = 10
-    min_width = int(round(min(chain(*[d['x'] for d in drawing])))) - padding
-    max_width = int(round(max(chain(*[d['x'] for d in drawing])))) + padding
+    min_width = int(round(min(chain(*[d["x"] for d in drawing])))) - padding
+    max_width = int(round(max(chain(*[d["x"] for d in drawing])))) + padding
     width = max_width - min_width
-    min_height = int(round(min(chain(*[d['y'] for d in drawing])))) - padding
-    max_height = int(round(max(chain(*[d['y'] for d in drawing])))) + padding
+    min_height = int(round(min(chain(*[d["y"] for d in drawing])))) - padding
+    max_height = int(round(max(chain(*[d["y"] for d in drawing])))) + padding
     height = max_height - min_height
 
     # Draw image
     im = Image.new("RGBA", (width * AA, height * AA))
     draw = ImageDraw.Draw(im)
-    for line in drawing:
-        len_line = len(line['x'])
-        points = [
-            (
-                (line['x'][i] - min_width) * AA,
-                (line['y'][i] - min_height) * AA
+    for coords in drawing:
+        line_length = len(coords["x"])
+        if line_length == 1:
+            # This is a single point, convert to a circle of 2x2 AA pixels
+            draw.ellipse(
+                [
+                    (
+                        (coords["x"][0] - min_width) * AA,
+                        (coords["y"][0] - min_height) * AA,
+                    ),
+                    (
+                        (coords["x"][0] - min_width + 2) * AA,
+                        (coords["y"][0] - min_height + 2) * AA,
+                    ),
+                ],
+                fill="#000",
             )
-            for i in range(0, len_line)
-        ]
-        draw.line(points, fill="#000", width=2 * AA)
+        else:
+            points = [
+                ((coords["x"][i] - min_width) * AA, (coords["y"][i] - min_height) * AA)
+                for i in range(0, line_length)
+            ]
+            draw.line(points, fill="#000", width=2 * AA)
     im = ImageOps.expand(im)
     # Smart crop
     bbox = im.getbbox()
@@ -59,11 +72,11 @@ def draw_signature(data, as_file=False):
     im.thumbnail(
         (width, height),
         # Image.ANTIALIAS is replaced in PIL 10.0.0
-        Image.ANTIALIAS if int(PIL_VERSION.split('.')[0]) < 10 else Image.LANCZOS
+        Image.ANTIALIAS if int(PIL_VERSION.split(".")[0]) < 10 else Image.LANCZOS,
     )
 
     if as_file:
-        ret = im._dump(format='PNG')
+        ret = im._dump(format="PNG")
     else:
         ret = im
 
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -4,16 +4,33 @@ from django.test import SimpleTestCase
 
 from jsignature.templatetags.jsignature_filters import signature_base64
 
-DUMMY_VALUE = [{"x": [205, 210], "y": [59, 63]},
-               {"x": [205, 207], "y": [67, 64]}]
+DUMMY_VALUE = [{"x": [205, 210], "y": [59, 63]}, {"x": [205, 207], "y": [67, 64]}]
 DUMMY_STR_VALUE = json.dumps(DUMMY_VALUE)
 
 
 class TemplateFilterTest(SimpleTestCase):
     def test_inputs_bad_type_value(self):
-        self.assertEqual(signature_base64(object()), '')
-        self.assertEqual(signature_base64(None), '')
+        self.assertEqual(signature_base64(object()), "")
+        self.assertEqual(signature_base64(None), "")
 
     def test_outputs_as_base64(self):
         output = signature_base64(DUMMY_STR_VALUE)
-        self.assertEqual(output, "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAcCAYAAACUJBTQAAAAuElEQVR4nO3TMQ4BQRSH8d8uR9AQN5Do9Bo3wB3cQCs6lULcwZkkGmeQSBQU+yZEVIxC7FfNvGTnm7fvP9TU/A0NFN8UPB5eflMwQjvWzZyCMiQ9XLFHP7eoCFEL2xCdMMkleMUMl5AtVN1km1GhShcMcQjRKmqNF9+8JSnd59HFDoPYf9xNuuUUZ8w/PfCZlK4OjqpfNI5aU6bHmWK6DsEm9llmkCjcO1mqopxqv0mK8O92UFPzPjdRMBNmBFDqcwAAAABJRU5ErkJggg==")
+        self.assertEqual(
+            output,
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAcCAYAAACUJBTQAAAAuElEQVR4nO3TMQ4BQRSH8d8uR9AQN5Do9Bo3wB3cQCs6lULcwZkkGmeQSBQU+yZEVIxC7FfNvGTnm7fvP9TU/A0NFN8UPB5eflMwQjvWzZyCMiQ9XLFHP7eoCFEL2xCdMMkleMUMl5AtVN1km1GhShcMcQjRKmqNF9+8JSnd59HFDoPYf9xNuuUUZ8w/PfCZlK4OjqpfNI5aU6bHmWK6DsEm9llmkCjcO1mqopxqv0mK8O92UFPzPjdRMBNmBFDqcwAAAABJRU5ErkJggg==",
+        )
+
+    def test_outputs_as_base64_with_singlepoints(self):
+        face = [
+            {"x": [117], "y": [88]},
+            {"x": [140], "y": [83]},
+            {
+                "x": [116, 121, 125, 129, 134, 139, 142, 145],
+                "y": [100, 100, 101, 102, 102, 102, 100, 97],
+            },
+        ]
+        output = signature_base64(json.dumps(face))
+        self.assertEqual(
+            output,
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADEAAAAnCAYAAACmE6CaAAABmklEQVR4nO3XvUpcQRjG8d9xhYghhYFoIZgmbSCVQlIERAKpAl6GuYrchNeQFLmCRLGytdDCIpUEAmkNxIAfKWYGDrK6c9adXTeZP7zs7izn/ZiZ5505VCqVSuU/oIn2TzCVhbSTfjCxLO5Igzns4BuW49hMroPZMnllk1ahh2dYwUNcKbitSogv+VsSCmmPFaVkkKIrkCglvk46GDbAncRXkhxhT0R8JZiY+Eoy1QXcGx1UxkQjNJKppa23qdy6KekFbMbvI20iTR/rCWfNzAiCpQLm8VU4h97HsbFurWGLSofuPL4IBZzinYyOOOjEbqLDR/gcP69a/51gD/s4xp8+zw+iwblwJ/uIDfyKBezGAi4z/NwaAB4Lyd9mJ/iELbzQ7aL4EgfRzw+sx/Gs953cJe/hectpem4Br/Aaa8IlMfE9WlrNm+I3WI2/t/EBP2PMi8z8RsYi3sQk9vDb4NVLdoi3LV+dWmsX8d0k1kaYseuz/QRPB/hMN+EjnAmzf9nH19i43nK7MnQbLf2amVvMRGe/UqlUyvAXHBNDpT7g8gsAAAAASUVORK5CYII=",
+        )
